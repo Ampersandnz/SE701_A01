2fc2d9393aa5ab364da7deffb194d6c2782efe3e
Michael Lo <nz.ampersand@gmail.com>
2015-03-07 11:37:53 +1300

    Forgot to initialise repository as blank. Scanner is complete but needs more testing, parser has not been started.

---
+++ src/ScannerMain.java
@@ -0,0 +1,37 @@
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+import uni.SimpleCharStream;
+import uni.Token;
+import uni.UniParserTokenManager;
+import uni.UniParserConstants;
+
+public class ScannerMain {
+
+	public static void main(String[] args) {
+
+		for (int i = 1; i <= 1; i++ ) {
+			File f = new File("tests" + System.getProperty("file.separator") + "input" + i + ".txt");
+
+			FileInputStream is;
+			try {
+				is = new FileInputStream(f);
+
+				UniParserTokenManager scanner = new UniParserTokenManager(new SimpleCharStream(is));
+				
+				Token t;
+				
+				while ((t = scanner.getNextToken()).kind != UniParserConstants.EOF) {
+					System.out.println(t.image);
+				}
+				
+				System.out.println("input" + i + " Done");
+				
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
